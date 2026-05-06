0a7aa7005701ed19aa4bf75ffc5b614cfbb52370
pavankumar <pavankumar.gspk9@gmail.com>
2026-05-06 12:31:07 +0530

    Inventory Service Created ---> First Commit

---
+++ src/main/java/com/example/practice/resttemplate/controller/InventoryController.java
@@ -0,0 +1,16 @@
+package com.example.practice.resttemplate.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/inventory")
+public class InventoryController {
+
+	@GetMapping("/{productID}")
+	public String checkInventory(@PathVariable(name = "productID") String productID) {
+		return "1".equals(productID) ? "InStock" : "Out Of Stock";
+	}
+}
---
+++ src/test/java/com/example/practice/resttemplate/EcomInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice.resttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
